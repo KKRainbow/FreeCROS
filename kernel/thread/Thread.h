441c98d51a25b0f1f64bcf41f0123e27134d294b
孙思杰 <443152178@qq.com>
2015-06-14 11:12:18 +0800

    重新安排了一下Thread class 的代码布局，不然显得太乱

--- kernel/thread/Thread.h
+++ kernel/thread/Thread.h
@@ -18,62 +18,56 @@ class Thread
 {
 public:
 	friend class ThreadManager;
+
+
+
+
+	
+	/*********************************************************************/
+	//进程的主要组成部件//////////////////////////////////////
+private:
+	lr::sstl::List<Thread*> children;
+	Thread* father = nullptr;
+	
+	lr::Ptr<AddressSpace> addressSpace; 
+	CPUState cpuState;
+	
+	lr::Ptr<ThreadState> state;
+	static const int MAX_THREAD = 1024;
+	lr::Ptr<uint8_t> childBitMap; //It serves to allocate stack space
+public:
+	SpinLock threadLock;
+	lr::Ptr<AddressSpace>& GetAddressSpace();
+	lr::Ptr<ThreadState>& State();
+	void ClockNotify(uint64_t _Counter);
+	//进程的主要组成部件//////////////////////////////////////
+	/*********************************************************************/
+	
+
+
+
+
+
+
+	
+	/*********************************************************************/
+	//进程体重要信息相关///////////////////////
 private:
 	pid_t pid;
 	int priority;
+	ThreadType threadType;
 	uint32_t alarmCounter = 0;
 	uint32_t kernelCounter = 0;
 	uint32_t userCounter = 0;
 	uint32_t cpuCounter = 8000;
-	lr::Ptr<AddressSpace> addressSpace; 
-	CPUState cpuState;
-	lr::sstl::List<Thread*> children;
-	Thread* father = nullptr;
-	lr::Ptr<ThreadState> state;
+	
 	uint32_t stackAddr = 3<<30;  //Every thread has its own stack ,3GB Pos
 	uint32_t stackSize = 1024*8;	//32MB
-	static const int MAX_THREAD = 1024;
-	lr::Ptr<uint8_t> childBitMap; //It serves to allocate stack space
-	//Pair of pid and IPCMessage
-	lr::sstl::Map<int,lr::sstl::Queue<IPCMessage>> msgMap;
-	ThreadType threadType;
 	
 	addr_t kernelStackAddr = 0;
 	size_t kernelStackSize = 4 << PAGE_SHIFT;
-	
-	SpinLock msgLock;
-	SpinLock sigLock;
-	
-	//信号有关
-	//信号值,信号结构体
-	lr::sstl::MultiMap<int,pid_t> sigmap;
-	lr::sstl::Map<int,sigaction> sigactions;
-	CPUState beforeSignal;
-	uint32_t mask;
-	addr_t signalKernelStackAddr = 0;
-	addr_t signalKernelStackSize = 1 << PAGE_SHIFT;
-	bool isSignalProcessFinish = false;
-	bool isSignalProcessing = false;
 public:
-	void Alarm(uint32_t _Us);
-private:
-	///////////////
-// private:
-public:
-	Thread& operator=(const Thread&){return *this;}
-	Thread(const Thread&){}
-	//Create a thread with a independent address space=
-	Thread(pid_t pid,ThreadType _Type,Thread* _Father = nullptr);		
-	//Create a thread with a dependent address space
-	Thread(Thread& _Thread,int _Pid);
-	Thread(){};
-	void ClockNotify(uint64_t _Counter);
-public:
-	WaitableObj waitSelf;
-	WaitableObj waitIPCSend;
-	WaitableObj waitIPCReceive;
 	int cpuRunningOn = 0;
-	
 	pid_t GetPid();
 	uint32_t& CPUCounter();
 	uint32_t& KernelCounter();
@@ -84,31 +78,78 @@ public:
 	
 	void IncKernelCounter(uint32_t _Inc);
 	void IncUserCounter(uint32_t _Inc);
+	//进程体重要信息相关///////////////////////
+	/*********************************************************************/
 	
-	lr::Ptr<AddressSpace>& GetAddressSpace();
-	lr::Ptr<ThreadState>& State();
+
+
+
+
 	
-	void SetEntry(addr_t _Addr);
-	
-	bool ReceiveMessage(IPCMessage& _Msg);
-	bool ExtractMessage
-	(int _Pid,IPCMessage& _Msg);//Get the ipc msg of one spcific thread
-	bool ExtractMessage(IPCMessage& _Msg);
-	
-	bool PrepareToRun();	//Do some check before running. Handle signal~~
-	
-	template<class T>
-	void PushUserStack(T val,addr_t& stack_Addr);
-	template<class T>
-	T PopUserStack();
-	
+	/*********************************************************************/
+	//信号有关,信号值,信号结构体///////////////////////////////////
+private:
+	SpinLock sigLock;
+	lr::sstl::MultiMap<int,pid_t> sigmap;
+	lr::sstl::Map<int,sigaction> sigactions;
+	CPUState beforeSignal;
+	uint32_t mask;
+	addr_t signalKernelStackAddr = 0;
+	addr_t signalKernelStackSize = 1 << PAGE_SHIFT;
+	bool isSignalProcessFinish = false;
+	bool isSignalProcessing = false;
+public:
+	void Alarm(uint32_t _Us);
 	addr_t AddSignalHandler(int _Signum,addr_t _Handler,int _Flag);
 	bool Kill(pid_t _Source,int _Signum);
 	bool RestoreFromSignal();
 	bool IsProcessingSignal(){return this->isSignalProcessing;};
+	bool PrepareToRun();	//Do some check before running. Handle signal~~
+	template<class T>
+	void PushUserStack(T val,addr_t& stack_Addr);
+	template<class T>
+	T PopUserStack();
+	//信号有关///////////////////////////////////
+	/*********************************************************************/
 	
+
+
+
+
+	
+	/*********************************************************************/
+	//消息传递相关////////////////////////////////////////////
+private:
+	//Pair of pid and IPCMessage
+	lr::sstl::Map<int,lr::sstl::Queue<IPCMessage>> msgMap;
+	SpinLock msgLock;
 public:
-	SpinLock threadLock;
+	WaitableObj waitSelf;
+	WaitableObj waitIPCSend;
+	WaitableObj waitIPCReceive;
+	bool ReceiveMessage(IPCMessage& _Msg);
+	bool ExtractMessage
+	(int _Pid,IPCMessage& _Msg);//Get the ipc msg of one spcific thread
+	bool ExtractMessage(IPCMessage& _Msg);
+	/*********************************************************************/
+	
+	
+	
+	
+	
+	/*********************************************************************/
+public:
+	//线程构造/////////////////////////////////
+	Thread& operator=(const Thread&){return *this;}
+	Thread(const Thread&){}
+	//Create a thread with a independent address space=
+	Thread(pid_t pid,ThreadType _Type,Thread* _Father = nullptr);		
+	//Create a thread with a dependent address space
+	Thread(Thread& _Thread,int _Pid);
+	Thread(){};
+	void SetEntry(addr_t _Addr);
+	//线程构造/////////////////////////////////
+	/*********************************************************************/
 };
 
 template<class T>
